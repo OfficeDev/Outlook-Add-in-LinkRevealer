a15620769ae74908d5ce19ad988e06228d330a41
anbyrne <anbyrne@microsoft.com>
2015-08-28 17:38:22 -0700

    Use body.getAsync and reduce permissions back to ReadItem.

--- LinkRevealerWeb/AppRead/Home/Home.js
+++ LinkRevealerWeb/AppRead/Home/Home.js
@@ -13,79 +13,13 @@
         $(document).ready(function () {
             app.initialize();
 
-            retrieveMessageBody();
+            Office.context.mailbox.item.body.getAsync({ coercionType: "html" }, processHtmlBody);
         });
     };
 
-    // Displays the "Subject" and "From" fields, based on the current mail item
-    function retrieveMessageBody() {
-        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
-        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
-            sendRequest();
-        }
-    }
 
-    // Send an EWS request for the message's body. 
-    function sendRequest() {
-        var mailbox = Office.context.mailbox;
-        var request = getBodyRequest(mailbox.item.itemId);
-        var envelope = getSoapEnvelope(request);
-
-        mailbox.makeEwsRequestAsync(envelope, findLinksInMessageBody);
-    };
-
-    function getBodyRequest(id) {
-        // Return a GetItem EWS operation request for the subject of the specified item.  
-        var result =
-
-     '    <GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
-     '      <ItemShape>' +
-     '        <t:BaseShape>IdOnly</t:BaseShape>' +
-     '        <t:AdditionalProperties>' +
-     '            <t:FieldURI FieldURI="item:Body"/>' +
-     '        </t:AdditionalProperties>' +
-     '      </ItemShape>' +
-     '      <ItemIds><t:ItemId Id="' + id + '"/></ItemIds>' +
-     '    </GetItem>';
-
-        return result;
-    };
-
-    function getSoapEnvelope(request) {
-        // Wrap an Exchange Web Services request in a SOAP envelope. 
-        var result =
-
-        '<?xml version="1.0" encoding="utf-8"?>' +
-        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
-        '               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
-        '               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
-        '               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
-        '  <soap:Header>' +
-        '  <t:RequestServerVersion Version="Exchange2013"/>' +
-        '  </soap:Header>' +
-        '  <soap:Body>' +
-
-        request +
-
-        '  </soap:Body>' +
-        '</soap:Envelope>';
-
-        return result;
-    };
-    
-
-    // Function called when the EWS request is complete. 
-    function findLinksInMessageBody(asyncResult) {
-        var response = asyncResult.value;
-        var context = asyncResult.context;
-
-        // Process the returned response here. 
-        var responseSpan = document.getElementById("response");
-        var xmlDoc = $.parseXML(response);
-        var $xml = $(xmlDoc);
-
-
-        var htmlParser = new DOMParser().parseFromString($xml.text(), "text/html");
+    function processHtmlBody(asyncResult) {
+        var htmlParser = new DOMParser().parseFromString(asyncResult.value, "text/html");
         var links = htmlParser.getElementsByTagName("a");
         var phishyLinkCount = 0;
         var normalLinkCount = 0;
@@ -98,7 +32,7 @@
                     var hrefText = v.href.toLowerCase().trim();
                     var phishyIcon = "";
                     var linkIsPhishy = ((vInnerText.search("http") == 0) && vInnerText != hrefText);
-                    
+
                     if (linkIsPhishy) {
                         phishyLinkCount++;
                         $("#links-table").append("<div class='ms-Table-row ms-font-xs ms-bgColor-redDark ms-font-color-white'>" +
@@ -118,7 +52,7 @@
             );
 
         $('#result').append("Number of links found in this email: " + (normalLinkCount + phishyLinkCount) + " Number of phishy links (red): " + phishyLinkCount);
-    };
+    }
 
 })();
 
